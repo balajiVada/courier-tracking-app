1d5eff11291a2dd1954276d17fd9c1f6d597445a
balajiVada <vadabalaji@gmail.com>
2026-05-26 18:21:28 +0530

    Fix Aiven SSL self-signed certificate error

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : undefined,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 });
 
 const dbProm = db.promise();
